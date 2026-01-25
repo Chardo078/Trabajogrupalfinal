8a219146f0d95c6f249b41830d3be626f7648b2f
Chardo078 <richardgonzaga078@gmail.com>
2026-01-25 18:20:28 -0500

    Subida inicial del proyecto

--- src/main/java/com/programacion/avanzada/modelo/Book.java
+++ src/main/java/com/programacion/avanzada/modelo/Book.java
@@ -1,4 +1,17 @@
 package com.programacion.avanzada.modelo;
 
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+@Entity
+
 public class Book {
+
+    @Id
+    private String id;
+
+    @Column
+    private String nombre;
 }
